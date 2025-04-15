b6935e6d331144161bf1fd23d40bc8a7e2dfd064
Degefa Gomora <degefag21@gmail.com>
2025-04-15 21:57:43 +0300

    Final at tuesday

--- apple/src/main.jsx
+++ apple/src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-//CSS
+// CSS
 import "bootstrap/dist/css/bootstrap.min.css";
-//Js
+// Js
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./assets/js/bootstrap.js"
+
+
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
